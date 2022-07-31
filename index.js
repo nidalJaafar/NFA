fa79f35309763352399da3e18dc06b851a972a27
nidalJaafar <nidaljaafar.nj@gmail.com>
2022-07-31 11:25:33 +0300

    touch support

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const res = async regex => {
     const data = await fetch('https://darsanfa.azurewebsites.net/api/nfa/compile?regex=' + regex)
-    let text = await data.text();
-    const mermaidDiv = document.getElementById('mermaid');
-    mermaidDiv.innerHTML = 'flowchart LR\n' + text
+    const mermaidDiv = document.getElementById('mermaid')
+    mermaidDiv.innerHTML = 'flowchart LR\n' + await data.text()
     mermaidDiv.removeAttribute('data-processed')
     mermaid.init()
 }
 
-document.getElementById('form').addEventListener('submit', (e) => {
+['click', 'touchstart'].forEach(event => document.getElementById('generate').addEventListener(event, (e) => {
     e.preventDefault()
     res(document.getElementById('regex-input').value)
-})
+}))
 
 mermaid.initialize({startOnLoad: false})
